8da4c0e3985e62bb014385fb85062b304705ea7d
Adam Schnicker <adamschnicker@Adams-MacBook-Pro.local>
2018-06-23 23:00:20 -0400

    version 2

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Southwestern", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalapeno", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss", true);
+
